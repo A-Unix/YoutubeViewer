78eb77c182bc5845bea3aa5ce668d46765874b79
Atul93 <54507991+A-Unix@users.noreply.github.com>
2024-02-05 13:39:03 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,11 +4,26 @@ echo -e "Checking if the script is running as root!"
 
 # Check if script is running as root user
 if [[ $EUID -ne 0 ]]; then
-    echo "This script must be run as root. Please use 'sudo' or login as root."
+    echo "This script must be run as root. Please use 'sudo' along with the command or login as root user."
+    sleep 1
+    echo "Saving Preferences for next try!"
+    sleep 7
     exit 1
 fi
 
-echo -e "\nUpdating your system, please wait\n"
+# Check if Colorama has been already installed or not
+if python3 -c "import colorama" &>/dev/null; then
+    echo -e "\033[95mColorama has been already installed, We have initialized it for you :)\033[0m"
+    sleep 5
+else
+    echo -e "\033[91mColorama has not been installed. Installing it...\033[0m"
+    pip install colorama
+    echo -e "\033[95mDone, Colorama has been installed.\033[0m"
+    sleep 3
+fi
+
+
+echo -e "\033[93m\nUpdating your system, wait!\n\033[0m"
 sleep 0.5
 
 # Update system
@@ -17,7 +32,7 @@ apt update
 # Install pip
 apt install python3-pip -y
 
-echo -e "\nInstalling required dependencies, please wait!\n"
+echo -e "\033[96m\nInstalling required dependencies, wait!\n\033[0m"
 sleep 0.5
 
 # Install requirements to run the script
@@ -25,7 +40,7 @@ pip3 install stem
 pip3 install selenium
 apt install figlet lolcat tor -y
 
-echo -e "\nDone, run the 'main.py' file now!\n"
-echo -e "\nNote that you need to download 'Gecko Driver' which is compatible with your Linux OS before using 'main.py'."
+echo -e "\033[32m\nDone, run the 'main.py' file now!\n\033[0m"
+echo -e "\033[95m\nNote that you need to download 'Gecko Driver' which is compatible with your Linux OS before using 'main.py'.\033[0m"
 sleep 2
 
